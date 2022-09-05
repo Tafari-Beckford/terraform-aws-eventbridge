d15640f2d95d85d6b280abe92c67fac43baa07a9
Tafari-Beckford <112142923+Tafari-Beckford@users.noreply.github.com>
2022-09-05 17:53:46 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -35,7 +35,7 @@ output "eventbridge_connection_arns" {
 # EventBridge Destination
 output "eventbridge_api_destination_arns" {
   description = "The EventBridge API Destination ARNs created"
-  value       = try(aws_cloudwatch_event_api_destination.this[0].arn, "")
+  value       = try(aws_cloudwatch_event_api_destination.arn, "")
 }
 
 # EventBridge Rule
